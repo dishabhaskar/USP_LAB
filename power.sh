c6ddd852903c61b56b70547b100a4d58df791058
dishabhaskar <71321930+dishabhaskar@users.noreply.github.com>
2021-11-29 13:02:51 +0530

    Create power.sh

---
+++ power.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "Enter the number"
+read n
+echo "ENter the power"
+read p
+pow=$p
+mul=1
+while [ $p -gt 0 ]
+do 
+	mul=$((mul*n))
+	p=$((p-1))
+done
+echo "$n To THe Power $pow is $mul"
