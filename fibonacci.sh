3399f74a6868cd735c87d3444ea200faa900d7eb
dishabhaskar <71321930+dishabhaskar@users.noreply.github.com>
2021-11-29 14:26:55 +0530

    Create fibonacci.sh

---
+++ fibonacci.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "enter the number of terms:"
+read n
+a=0
+b=1
+i=2
+echo "fibonacci series:"
+echo "$a"
+echo "$b"
+while [ $i -lt $n ]
+do
+        i=`expr $i + 1`
+        c=`expr $a + $b`
+        echo "$c"
+        a=$b
+        b=$c
+done
+~     
